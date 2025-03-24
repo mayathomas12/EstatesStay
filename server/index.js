11badf283dac9070e50a2ea2724c0102a4da8cf0
mim <maya.thomas0000@gmail.com>
2025-03-25 00:53:59 +0600

    add new

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const listingRoutes = require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
 
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -21,7 +20,7 @@ app.use("/bookings", bookingRoutes)
 app.use("/users", userRoutes)
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
     dbName: "Estates_Stay",
